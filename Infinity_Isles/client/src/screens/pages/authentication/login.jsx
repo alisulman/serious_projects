7a21be0f64a07053d19ff443e3d11310c86fc8e3
Ali <alisulman1470@gmail.com>
2024-04-03 16:05:53 +0500

    add validation error in registraion form

--- Infinity_Isles/client/src/screens/pages/authentication/login.jsx
+++ Infinity_Isles/client/src/screens/pages/authentication/login.jsx
@@ -1,32 +1,61 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import * as React from 'react'
+import Icon from 'react-icons-kit'
 import { FcGoogle } from "react-icons/fc";
 import { TiSocialFacebook } from "react-icons/ti";
 import RegistrationWrap from "../../../components/wrapper/RegistrationWrap";
 import { Link } from 'react-router-dom'
+import { eye } from 'react-icons-kit/icomoon/eye'
+import { eyeBlocked } from 'react-icons-kit/icomoon/eyeBlocked'
 
-const Login = () => {
+const Signup = () => {
+    let [type, setType] = React.useState('password')
+
+
     return (
         <RegistrationWrap>
             <div className="container flex flex-col justify-center items-center mx-auto">
                 <h1 className="text-2xl font-bold sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">Login</h1 >
-                <h1 className="flex text-sm my-3 sm:text-base md:text-lg lg:text-xl xl:text-2xl">Already have an account?<Link to='/authentication/signup' className='flex items-center text-xs text-blue-600 sm:text-sm md:text-base lg:text-lg xl:text-xl'>Signup</Link></h1>
+                <h1 className="flex text-sm my-3 sm:text-base md:text-lg lg:text-xl xl:text-2xl">Don&apos;t have an account?<Link to='/authentication/signup' className='flex items-center text-xs text-blue-600 sm:text-sm md:text-base lg:text-lg xl:text-xl'>Signup</Link></h1>
             </div>
             <div className="flex flex-col justify-center items-center sm:flex-row sm:gap-5 md:gap-10 lg:gap-20">
-                <form className="w-80 sm:w-[290px] md:min-w-[300px] lg:min-w-[400px]">
-                    <div className="relative h-11 my-5">
-                        <input placeholder="Email"
+                <form className="w-80 sm:w-[290px] md:min-w-[300px] lg:min-w-[400px]" noValidate>
+                    <div className="relative h-11 my-8">
+                        <input
+                            placeholder="Email"
+                            id="email"
                             type="text"
                             name="email"
-                            className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100" />
+                            className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-400"
+                            required
+                            pattern="[^\s@]+@[^\s@]+\.[^\s@]+$"
+                        />
+                        <div className="hidden text-xs text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block  my-1">Please enter a valid email</div>
+                        <div></div>
+
                         <label
                             className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                             Email
                         </label>
                     </div>
-                    <div className="relative h-11 my-5">
-                        <input placeholder="Password"
-                            type="password"
+                    <div className="relative h-11 my-8">
+                        <input
+                            placeholder="Password"
+                            id="password"
+                            type={type}
                             name="password"
-                            className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100" />
+                            className="peer h-full w-full border-b border-blue-gray-200 bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 placeholder:opacity-0 focus:placeholder:opacity-100 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-400"
+                            required
+                            pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&]{8,}$"
+                        />
+                        {type === "password" ? (
+                            <span className='absolute text-gray-600 inset-y-0 end-0 flex items-center pe-3' onClick={() => setType("text")}><Icon icon={eye} /></span>
+                        ) : (
+                            <span className='absolute text-gray-600 inset-y-0 end-0 flex items-center pe-3' onClick={() => setType("password")}><Icon icon={eyeBlocked} /></span>
+                        )}
+                        <div className="hidden text-xs text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block  my-1">Please enter a valid password</div>
                         <label
                             className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-blue-gray-500 peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:after:scale-x-100 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                             Password
@@ -35,8 +64,10 @@ const Login = () => {
                     <div className="flex justify-end">
                         <button
                             type="submit"
-                            className="text-sm text-blue-800 font-medium bg-white  border border-blue-800 rounded-full w-full px-9 py-1.5 mt-3 focus:ring-1 focus:ring-blue-300 focus:outline-none hover:bg-blue-800 hover:text-white sm:text-[10px] md:text-xs lg:text-sm sm:px-5 md:px-7 lg:px-9 sm:py-0.5 md:py-1 lg:py-1.5 sm:my-1 md:my-2 lg:my-3">
-                            Signup
+                            id="submitBtn"
+                            className="text-sm text-blue-800 font-medium bg-white  border border-blue-800 rounded-full w-full px-9 py-1.5 mt-3 focus:ring-1 focus:ring-blue-300 focus:outline-none hover:bg-blue-800 hover:text-white sm:text-[10px] md:text-xs lg:text-sm sm:px-5 md:px-7 lg:px-9 sm:py-0.5 md:py-1 lg:py-1.5 sm:my-1 md:my-2 lg:my-3"
+                        >
+                            Login
                         </button>
                     </div>
                 </form>
@@ -60,9 +91,8 @@ const Login = () => {
                     </div>
                 </div>
             </div>
-
         </RegistrationWrap>
     )
 }
 
-export default Login
+export default Signup
